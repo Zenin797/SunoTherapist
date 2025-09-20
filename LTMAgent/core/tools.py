1c8d36194de68b887337ac75a3a445a6ebc46fbd
Kanha <devincog797@gmail.com>
2025-09-20 21:07:15 +0530

    little modifications

--- LTMAgent/core/tools.py
+++ LTMAgent/core/tools.py
@@ -28,11 +28,6 @@ try:
 except ImportError:
     LANGMEM_AVAILABLE = False
 
-get_memory_usage_tool = StructuredTool.from_function(
-    func=get_memory_usage,
-    description="Get the current system memory usage as a float type percentage."
-)
-
 search_internet_tool = SearxSearchResults(
     num_results=5,
     wrapper=SearxSearchWrapper(searx_host=get_config("searx_host"))
